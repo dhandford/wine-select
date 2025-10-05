d8ca9c9d70390890fa4ba16ca1fc76f8841409f9
dhandford <58715676+dhandford@users.noreply.github.com>
2025-10-05 04:04:48 -0700

    created footer

---
+++ src/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer style={{ textAlign: 'center', marginTop: 32, fontSize: 14, color: "#666" }}>
+            Wine Selector v{APP_VERSION}
+        </footer>
+    );
+}
