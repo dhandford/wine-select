b50062bb6e41e263ffb1cb7cf4dfa413acb7e3e3
dhandford <58715676+dhandford@users.noreply.github.com>
2025-10-05 04:19:19 -0700

    fixed footer.js agaion

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,9 @@
 import { APP_VERSION } from '../version';
+
+export default function Footer() {
+    return (
+        <footer style={{ textAlign: 'center', marginTop: 32, fontSize: 14, color: "#666" }}>
+            Wine Selector v{APP_VERSION}
+        </footer>
+    );
+}
